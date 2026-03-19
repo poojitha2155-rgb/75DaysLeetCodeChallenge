3306bc84671eb94b35723ddd258918f94b541e0c
Narayanareddygari Poojitha <poojitha2155@gmail.com>
2026-03-19 22:38:36 +0530

    Time: 11 ms (91.58%), Space: 12.6 MB (97.48%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            # Skip non-alphanumeric characters
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            # Compare characters
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
+        
